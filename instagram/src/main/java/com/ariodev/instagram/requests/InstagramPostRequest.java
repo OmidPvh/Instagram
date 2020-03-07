b9008dbd9d10f3b22465242bbc7c84962cbe4232
Omidpvh <Omid.p73@gmail.com>
2020-03-07 22:41:42 +0330

    1.1.45

--- instagram/src/main/java/com/ariodev/instagram/requests/InstagramPostRequest.java
+++ instagram/src/main/java/com/ariodev/instagram/requests/InstagramPostRequest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 import okhttp3.MediaType;
 import okhttp3.Request;
+import okhttp3.Request.Builder;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
@@ -29,19 +30,27 @@ public abstract class InstagramPostRequest<T> extends InstagramRequest<T>
     public T execute() throws IOException
     {
 
+        //        Request request = new Request.Builder().url(InstagramConstants.API_URL + getUrl())
+        //                                               .addHeader("Connection", "close")
+        //                                               .addHeader("Accept", "*/*")
+        //                                               .addHeader("Cookie2", "$Version=1")
+        //                                               .addHeader("Accept-Language", "en-US")
+        //                                               .addHeader("X-IG-Capabilities", "3boBAA==")
+        //                                               .addHeader("X-IG-Connection-Type", "WIFI")
+        //                                               .addHeader("X-IG-Connection-Speed", "-1kbps")
+        //                                               .addHeader("X-IG-App-ID", "567067343352427")
+        //                                               .addHeader("User-Agent", InstagramConstants.getUserAgent())
+        //                                               .post(RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"), InstagramHashUtil.generateSignature(getPayload())))
+        //                                               .build();
+
         Request request = new Request.Builder().url(InstagramConstants.API_URL + getUrl())
                                                .addHeader("Connection", "close")
                                                .addHeader("Accept", "*/*")
+                                               .addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
                                                .addHeader("Cookie2", "$Version=1")
                                                .addHeader("Accept-Language", "en-US")
-                                               .addHeader("X-IG-Capabilities", "3boBAA==")
-                                               .addHeader("X-IG-Connection-Type", "WIFI")
-                                               .addHeader("X-IG-Connection-Speed", "-1kbps")
-                                               .addHeader("X-IG-App-ID", "567067343352427")
                                                .addHeader("User-Agent", InstagramConstants.getUserAgent())
-                                               .post(RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"), InstagramHashUtil.generateSignature(getPayload())))
                                                .build();
-
 
         Response response = api.getClient()
                                .newCall(request)
