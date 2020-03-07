5e1f28a6e3fdeecb40eb91284767808495bb215d
Omidpvh <Omid.p73@gmail.com>
2020-03-08 00:36:22 +0330

    1.2.1

--- instagram/src/main/java/com/ariodev/instagram/requests/InstagramPostRequest.java
+++ instagram/src/main/java/com/ariodev/instagram/requests/InstagramPostRequest.java
@@ -64,7 +64,7 @@ public abstract class InstagramPostRequest<T> extends InstagramRequest<T>
             content = response.body()
                               .string();
         }
-        ////
+        Log.i("InstagramPostRequest", "execute: " + response.toString());
         return parseResult(resultCode, content);
     }
 }
